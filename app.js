ade1a61d226f9376ea55eebfe2bca5d21ac80cd1
Robsonsjre <robson.silva@revmob.com>
2016-11-01 18:23:15 -0200

    fodase

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 // load the things we need
 var express = require('express');
 var app = express();
+var http = require('http')
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
@@ -13,5 +14,12 @@ app.get('/', function(req, res) {
     res.render('pages/index');
 });
 
-app.listen(8080);
-console.log('8080 is the magic port');
+function startServer () {
+    var serverPort = process.env.OPENSHIFT_NODEJS_PORT || 8080
+    var ip         = process.env.OPENSHIFT_NODEJS_IP   || "127.0.0.1";
+    http.createServer(app).listen(serverPort, ip, null, function(){
+        console.log("Listening to http port " + serverPort)
+    })
+}
+
+startServer()
